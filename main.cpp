bd0d2a20444263410b2ddba12c0a15fd459bd39a
viditsmart <viditdsharma@gmail.com>
2026-07-07 20:18:45 -0500

    Added main menu

--- main.cpp
+++ main.cpp
@@ -4,6 +4,24 @@
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    int option;
+    
+    std::cout << "=========================" << std::endl;
+    std::cout << "RPG CHARACTER MANAGER" << std::endl;
+    std::cout << "=========================" << std::endl << std::endl;
+
+    std::cout << "1. Create Character" << std::endl;
+    std::cout << "2. View Characters" << std::endl;
+    std::cout << "3. Train Character" << std::endl;
+    std::cout << "4. Battle Simulation" << std::endl;
+    std::cout << "5. Heal Character" << std::endl << std::endl;
+    std::cout << "6. Delete Character" << std::endl;
+    std::cout << "7. Save Game" << std::endl;
+    std::cout << "8. Load Game" << std::endl;
+    std::cout << "9. Exit" << std::endl << std::endl;
+    std::cout << "Enter your choice: ";
+    std::cin >> option;
+
+
     return 0;
 }
